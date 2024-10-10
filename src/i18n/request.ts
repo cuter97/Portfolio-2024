f9c5f2f418c7c2620acd67040bd8f6e6cab6b8dd
cuter97 <aguscastets@gmail.com>
2024-10-10 16:41:52 -0300

    load locale messages from filesystem using fs module

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -2,11 +2,16 @@ import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
 import { routing } from './routing';
 
+import fs from 'fs';
+import path from 'path';
+
 export default getRequestConfig(async ({ locale }) => {
     // Validate that the incoming `locale` parameter is valid
     if (!routing.locales.includes(locale as 'en' | 'es')) notFound();
 
+    const filePath = path.join(process.cwd(), 'messages', `${locale}.json`);
+    const jsonData = fs.readFileSync(filePath, 'utf8');
     return {
-        messages: (await import(`../../messages/${locale}.json`)).default
-    };
+        messages: JSON.parse(jsonData)
+    }
 });
